37f45ee526c2dd448dd72ddd1c40ace7c1f72bac
digitaldrreamer <digitaldrreamer@gmail.com>
2025-05-15 16:53:36 +0100

    ✨ feat(Configuration): Updated Svelte configuration to enable edge functions and specified runtime settings. Simplified Vercel configuration by removing unnecessary build settings while retaining essential commands and region specifications.

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			// enable edge functions if needed
+			// edge: true,
+			runtime: 'edge',
+			external: [],
+			split: false
+		}),
 		alias: {
 			"@/*": "./path/to/lib/*",
 		},
